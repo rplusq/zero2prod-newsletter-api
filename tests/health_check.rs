97c9731d1d5ff077ebb9d9973f1a02e13b5ae432
Rafael Quintero <rafaplusq@gmail.com>
2023-08-08 22:44:30 +0100

    chore: test tasks run on available ports

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,14 +1,16 @@
 //! tests/health_check.rs
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
     // Act
     let response = client
-        .get("http://localhost:8000/health_check")
+        .get(format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -17,7 +19,11 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener: TcpListener =
+        TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
